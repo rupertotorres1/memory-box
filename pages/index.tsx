916d70bd3e739ec4170e798aded089356774a409
ruperto <rupertotorres1@gmail.com>
2020-07-26 22:41:36 -0500

    fix import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Head from 'next/Head';
-import Achievements from '../components/Achievements';
+import React from "react";
+import Head from "next/head";
+import Achievements from "../components/Achievements";
 
 const Home = () => {
   return (
